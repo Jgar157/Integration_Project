13892a78056c17da517004bfc6c94c533f3cc9d1
Jairo Garciga <jgarciga0822@eagle.fgcu.edu>
2019-09-12 14:23:10 -0400

    Initial commit

---
+++ BooleanStuff.py
@@ -0,0 +1,9 @@
+
+class BooleanStuff:
+   # Assign a value to weight and cost. Write a Boolean expression that tests if
+   # the value stored in weight is < 10 and the value store in cost is not greater than 20.00
+
+   weight = 100
+   cost = 50
+   weightTester = weight < 10
+   costTester
